9685953ffc3d55927d14475fedbe38d25efe3ffe
ivanjimenez <ivanjimenezfp@gmail.com>
2025-10-24 10:16:31 +0200

    .

---
+++ T2/productosrest01/src/test/java/org/example/productosrest01/Productosrest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.productosrest01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Productosrest01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
