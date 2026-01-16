e1d4ae9619c3f517b9f11bc7cfaf2243990f318a
ivanjimenez <ivanjimenezfp@gmail.com>
2026-01-16 07:27:23 +0100

    .

---
+++ T2/angular/todo-app/src/app/tarea-lista/tarea-lista.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarea-lista',
+  imports: [],
+  templateUrl: './tarea-lista.html',
+  styleUrl: './tarea-lista.scss',
+})
+export class TareaLista {
+
+}
